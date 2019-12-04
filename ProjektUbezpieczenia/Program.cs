906a2dd88e2e6aed7138ff6697dfafacea105ba0
AniaS98 <44135167+AniaS98@users.noreply.github.com>
2019-12-05 00:48:38 +0100

    Functions' repairs

--- ProjektUbezpieczenia/Program.cs
+++ ProjektUbezpieczenia/Program.cs
@@ -11,7 +11,8 @@ namespace ProjektUbezpieczenia
     {
         static void Main(string[] args)
         {
-            Klient K1 = new Klient("Anna", "Szmit", "12345678910", 21, Plcie.K, "123456798", false, Zawody.pracownik_biurowy, Pasje.gry_komputerowe);
+            Klient K1 = new Klient("Anna", "Szmit", "12345678910", 21, Plcie.K, "123456798", false, Zawody.pracownik_biurowy);
+            K1.DodajHobby(Pasje.gry_komputerowe);
             ListaKlientow listak = new ListaKlientow();
             listak.DodajKlienta(K1);
 
@@ -23,7 +24,9 @@ namespace ProjektUbezpieczenia
             //Console.WriteLine(podzial);
 
             K1.FunkcjaPakietDodatkowy(10, K1);
-
+            
+            K1.historia[K1.historia.Count - 1].PakietKoncowy.Podzialskl = 1;
+            K1.PakietPodstawowyIndywiduany(10, K1);
 
             listak.ZapiszXML();
 
@@ -39,6 +42,7 @@ namespace ProjektUbezpieczenia
         }
         static void NadpisywanieDanychKlienci(Klient k)
         {
+            List<double> results = new List<double>();
             int doros = 1;
             int ldzieci = 0;
             if (k.Rodzina != null)
@@ -48,8 +52,10 @@ namespace ProjektUbezpieczenia
             {
                 ldzieci--;
                 doros++;
+                results = k.PakietRodzinny(10, k);
             }
-
+            else
+                results = k.PakietPodstawowyIndywiduany(10, k);
 
             int[] wdzieci = new int[4];
             wdzieci[0] = 0;
@@ -70,33 +76,39 @@ namespace ProjektUbezpieczenia
                         wdzieci[2]++;
                 }
             }
+
             //SpE,Onk,Ort,PZD,NDZ,SWK,SNNW
             List<String> dodat = new List<string>();
-            {
-                int index = k.historia.Count - 1;
-
+            int index = k.historia.Count - 1;
+            foreach (var w in Enum.GetNames(typeof(Edodat)))
+            {  
                 dodat.Add("Nie");
-                foreach (var w in Enum.GetNames(typeof(Edodat)))
+                List<PakietDodatkowy> plista = k.historia[index].PakietKoncowy.Dodatkowe;
+                foreach (PakietDodatkowy pkd in plista)
                 {
-                    List<PakietDodatkowy> plista = k.historia[index].PakietKoncowy.Dodatkowe;
-                    foreach (PakietDodatkowy pkd in plista)
-                    {
                         if (pkd.Nazwa == w.ToString())
-                            dodat[dodat.Count - 1] = "Tak";
-                    }
+                        dodat[dodat.Count-1] = "Tak";
                 }
-                Console.WriteLine(dodat.Count);
             }
+            Console.WriteLine(dodat.Count);
+
+            string pod;
+            if (k.historia[index].PakietKoncowy.Podzialskl == 12)
+                pod = "Miesięczna";
+            else
+                pod = "Roczna";
 
 
-
-
+            Console.WriteLine(k.historia[index].PakietKoncowy.Lata);
 
             var wpis = new StringBuilder();
-            //var newLine = string.Format("41," + k.Imie + "," + k.Nazwisko.ToString() + "," + k.Wiek.ToString() + "," + k.Plec.ToString() + "," + ldzieci.ToString() + "," + wdzieci[0].ToString() + "," + wdzieci[1].ToString() + "," + wdzieci[2].ToString() + "," + doros.ToString() + "," + (doros + wdzieci[3]).ToString() + "," + dodat[1].ToString() + "," + dodat[2].ToString() + "," + dodat[3].ToString() + "," + dodat[4].ToString() + "," + dodat[5].ToString() + "," + dodat[6].ToString() + "," + dodat[7].ToString() + ",");
-            var newLine = string.Format("meh");
+            var newLine = string.Format("41," + k.Imie + "," + k.Nazwisko.ToString() + "," + k.Wiek.ToString() + "," + k.Plec.ToString() + "," +
+                ldzieci.ToString() + "," + wdzieci[0].ToString() + "," + wdzieci[1].ToString() + "," + wdzieci[2].ToString() + "," + doros.ToString() + "," + 
+                (doros + wdzieci[3]).ToString() + "," + dodat[0].ToString() + "," + dodat[1].ToString() + "," + dodat[2].ToString() + "," + dodat[3].ToString() + 
+                "," + dodat[4].ToString() + "," + dodat[5].ToString() + "," + dodat[6].ToString() + ","+pod + "," + results[0] + "," + results[1] + "," + 
+                k.historia[index].PakietKoncowy.Lata);
             wpis.Append(newLine);
-            using (FileStream fs = new FileStream("D:\\adm\\Documents\\Programowanie_C#\\Ubezpieczenia\\Ubezpieczenia\\Ubezpieczenia\\DaneKlienci.csv", FileMode.Append, FileAccess.Write))
+            using (FileStream fs = new FileStream("D:\\adm\\Documents\\Programowanie_C#\\ProjektUbezpieczenia\\ProjektUbezpieczenia\\DaneKlienci.csv", FileMode.Append, FileAccess.Write))
             {
                 using (StreamWriter sw = new StreamWriter(fs))
                 {
